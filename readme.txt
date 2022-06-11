7445aa4b324447fe383cce001090cf863c968803
Ahmed Moustafa <90020418+a-mufasa@users.noreply.github.com>
2022-06-10 20:35:06 -0500

    Add files via upload

---
+++ readme.txt
@@ -0,0 +1,62 @@
+Name: Ahmed Moustafa
+Uark ID: 010853047
+Operating System HW9: Sleeping Barber Problem
+
+The goal of the sleeping barber program is to solve the Sleeping Barber Problem as given in the assignment
+instructions using semaphores, mutex, and threads to properly implement concurrency. The code is split into
+5 classes (sleepingBarber, Barber, Customer, CustomerGenerator, and barberShop) and each part's description
+and role is shown below:
+
+sleepingBarber class:
+
+We create the program's main here to allow us to run the program as a whole. Within the main we initalizise and
+print our input variables and catch any input errors to avoid mis-inputs (make sure proper input values are 
+provided). An example of a case we'd catch is when only 1 input is given. In this case we ask for proper input 
+values andexit the system since that does not meet the given usage. The main also instantiates our shop, barber,
+and customer generator so that we can create and start the 1 barber thread and multiple customer threads.
+
+---------------------------------------------------------------------------------------------------------------
+
+Barber class:
+
+The Barber class contains a barberShop object so that we can put our 1 barber to sleep for their intitial 
+inputted sleep time and call cutHair() for the shop. We also implement Runnable and override run() since the 
+Barber object becomes a thread.
+
+---------------------------------------------------------------------------------------------------------------
+
+Customer class:
+
+Similar to the Barber class, we have a barberShop object so that we can add our customers to the waitingRoom
+queue in the barberShop class. We also implement Runnable and override rin() since the Customer object becomes 
+a thread. Each customer has a specific name and date for when they enter the shop.
+
+---------------------------------------------------------------------------------------------------------------
+
+CustomerGenerator class:
+
+Similar to the previous 2 classes we implement Runnable and override run() since CustomerGenerator becomes a
+thread that starts the multiple customer threads. We create and assign names/dates to customers in a while loop
+that generates new customer objects w/ random sleep times between 0 and 2 seconds.
+
+---------------------------------------------------------------------------------------------------------------
+
+barberShop class:
+
+In this class we create our semaphores and the queue that contains the waiting customers so that we can do the 
+concurrency part of the project. The constructor takes in the user-inputs so that we can use those values within
+our functions. 
+
+cutHair function:
+
+The cutHair() function updates the state of the barber (awake or asleep) and uses the semaphores and mutex 
+to access/manilpulate the customers, barber, and waiting (# of customers in waitingRoom). We also remove the 
+oldest element (FIFO) of the queue (customer leaves the shop) after a 3 second sleep which simulates hair 
+cutting. Lastly, we put the barber to sleep for the inputted sleep time if he finishes cutting a customer's
+hair and there are no remaining customers in the shop/waitingRoom.
+
+add function:
+
+The add(Customer customer) function uses the semaphores and mutex to access/manilpulate the customers, barber, 
+and waiting (# of customers in waitingRoom). If there are empty chairs in the waitingRoom we add the customer
+to the queue. Otherwise the customer leaves the shop.
---
+++ sleepingBarber.java
@@ -0,0 +1,227 @@
+//Name: Ahmed Moustafa
+//Uark ID: 010853047
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.Semaphore;
+import java.util.Random;
+import java.util.Date;
+
+
+public class sleepingBarber{
+// main is here
+	public static void main (String[] args){
+// Input sleepTimeBarber and numChairs from command line
+// Default sleepTimeBarber = 5, default numChairs = 3
+// Print parameters.
+// instantiate shop here.
+		int sleepTimeBarber = 0;
+		int numChairs = 0;
+		
+		System.out.println("Useage: <sleep time of barber> <number of chairs in waiting room>");
+		
+		if (args.length == 2){
+			try {
+				sleepTimeBarber = Integer.parseInt(args[0]);
+				numChairs = Integer.parseInt(args[1]);
+				
+				if (sleepTimeBarber < 0 || numChairs < 0){
+					System.out.println("Include proper input values");
+					System.exit(0);
+				}
+				else if(sleepTimeBarber == 5 && numChairs == 3) {
+					System.out.println("Using defaults");
+				}
+			}
+			catch(Exception e){
+				System.out.println("Include proper input values");
+			}
+		}
+		else if (args.length == 0){
+			sleepTimeBarber = 5;
+			numChairs = 3;
+			System.out.println("Using defaults");
+		}
+		else if(args.length > 2 || args.length == 1){
+			System.out.println("Include proper input values");
+			System.exit(0);
+		}
+		
+		System.out.println("Sleep time of barber = " + sleepTimeBarber);
+		System.out.println("Number of chairs in waiting room = " + numChairs + "\n");
+		
+		barberShop shop = new barberShop(numChairs, sleepTimeBarber);
+		Barber barber = new Barber(shop);
+		CustomerGenerator custGen = new CustomerGenerator(shop);
+		Thread oneBarber = new Thread(barber);
+		Thread multipleCustGen = new Thread(custGen);
+		oneBarber.start();
+		multipleCustGen.start();
+	}
+}
+
+// Barber object that will become thread.
+class Barber implements Runnable{
+	barberShop shop;
+	// Need access to shop object.
+	public Barber(barberShop shop){
+		this.shop = shop;
+	}
+	public void run(){
+		//Simulate sleep by putting thread to sleep
+		try{
+			Thread.sleep(shop.sleepTimeBarber*1000);
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
+		while(true){
+			shop.cutHair();
+		}
+	}
+}
+
+//Customer object that will become thread.
+class Customer implements Runnable{
+	barberShop shop;
+	Date enterTime;
+	String name;
+	
+	//Need access to shop object.
+	public Customer(barberShop shop){
+		this.shop = shop;
+	}
+	public String getName(){
+		return name;
+	}
+	public Date getEnterTime(){
+		return enterTime;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setEnterTime(Date enterTime){
+		this.enterTime = enterTime;
+	}
+	public void run(){
+		goForHairCut();
+	}
+	private void goForHairCut(){
+		shop.add(this);
+	}
+}
+
+//CustomerGenerator that will become thread to start customer threads.
+class CustomerGenerator implements Runnable{
+	barberShop shop;
+	int custID = 1; //Customer names start at 1
+	//Need access to shop object.
+	public CustomerGenerator(barberShop shop){
+		this.shop = shop;
+	}
+	public void run(){
+		while(true){
+			//Create customers and pass object "shop"
+			//Create thread
+			//start threads
+			//sleep random amount of time
+			Random random = new Random();
+			Customer customer = new Customer(shop);
+			customer.setEnterTime(new Date());
+			Thread custThread = new Thread(customer);
+			customer.setName("Customer Thread " + custID);
+			custThread.start();
+			custID++;
+			try{
+				custThread.sleep(random.nextInt(2*1000)); //0-2 seconds for customer generation
+			}
+			catch(InterruptedException e){
+				e.printStackTrace();
+			}
+		}
+	} 
+}
+
+class barberShop{
+	// Initialize variables
+	// Initialize semaphores and mutex and waiting
+	Queue<Customer> waitingRoom;
+	static Semaphore customersReady = new Semaphore(0);
+	static Semaphore barberReady = new Semaphore(0);
+	static Semaphore mutex = new Semaphore(1);
+	int waiting = 0;
+	int nChairs = 0;
+	int sleepTimeBarber;
+	boolean asleep = true;
+	
+	public barberShop(int nChairs, int sleepTimeBarber) {
+		this.nChairs = nChairs;
+		this.sleepTimeBarber = sleepTimeBarber;
+		waitingRoom = new LinkedList<Customer>();
+	}
+	
+	public void cutHair(){
+		// Wait on customer
+		// Do things here like update number of customers waiting, signal to wake up barber, etc.
+		// Simulate cutting hair with sleep
+		try{
+			if (asleep == true){
+				System.out.println("Barber started..");
+				asleep = false;
+			}
+			System.out.println("Barber is waiting for customer.");
+			customersReady.acquire();
+			System.out.println("Barber waiting for lock.");
+			mutex.acquire();
+			waiting = waiting - 1;
+			barberReady.release();
+			System.out.println("Barber found a customer in the queue.");
+			mutex.release();
+			System.out.println("Cutting hair of Customer : " + waitingRoom.peek().getName());
+			waitingRoom.remove();
+			try{
+				Thread.sleep(3*1000); //3 seconds to cut hair
+			}
+			catch (InterruptedException e){
+				e.printStackTrace();
+			}
+			if (waitingRoom.size() == 0){
+				try{
+					System.out.println("Barber went to sleep while waiting on customer.");
+					asleep = true;
+					Thread.sleep(sleepTimeBarber*1000);
+				}
+				catch (InterruptedException e){
+					e.printStackTrace();
+				}
+			}
+		}
+		catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void add(Customer customer){
+		// Wait on entering the critical section
+		// DO THINGS HERE like determine if there are enough chairs in the waiting room. Leave if waiting room is full. If waiting room isnot full, things must happen.
+		try{
+			System.out.println("Customer : " + customer.getName() + " entering the shop at " + customer.getEnterTime());
+			mutex.acquire();
+			if (waiting < nChairs){
+				System.out.println("Customer : " + customer.getName() + " got a chair."); //check that customer doesnt go to waiting room if barber not busy
+				waiting = waiting + 1;
+				customersReady.release();
+				mutex.release();
+				waitingRoom.add(customer);
+				barberReady.acquire();
+			}
+			else{
+				System.out.println("*** No chair available for customer " + customer.getName());
+				System.out.println("*** Customer " + customer.getName() + " Exits...");
+				mutex.release();
+			}
+		}
+		catch (InterruptedException e){
+			e.printStackTrace();
+		}
+	}
+}
